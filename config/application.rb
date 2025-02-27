c3a35e388019347284f81e6299d7a8e6ce09fee3
nanda.suhendra <nandhasuhendra@gmail.com>
2025-02-28 00:00:41 +0700

    chore: Update application config
    - Set time_zone to "Kuala Lumpur" which mean for UTC +8
    - Set active record timezone to follow the local setup
    - Set default localization (i18n) to English (:en)

--- config/application.rb
+++ config/application.rb
@@ -33,12 +33,16 @@ module GoodNight
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Kuala Lumpur"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Additional application setup
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :en
   end
 end
