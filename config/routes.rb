ce7d294f0db26bdde3b4bb24459b018fdeed0e30
nanda.suhendra <nandhasuhendra@gmail.com>
2025-02-28 19:32:45 +0700

    feat: Add localization message and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api, format: :json do
+    namespace :v1 do
+      post :login, to: "session#create"
+    end
+  end
 end
