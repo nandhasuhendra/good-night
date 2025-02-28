1f4c48d3ad55b77d818db7587eb281d77e8e7f09
nanda.suhendra <nandhasuhendra@gmail.com>
2025-03-01 00:15:19 +0700

    feat: Added association between user and follow models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true, uniqueness: true
+
+  has_many :follows, foreign_key: :follower_id, dependent: :destroy
+  has_many :followings, through: :follows, source: :followed
 end
