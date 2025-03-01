1200fef8218c54edee3096a0d9a1d6485a8a0702
nanda.suhendra <nandhasuhendra@gmail.com>
2025-03-02 01:38:29 +0700

    feat: Generate a new model to trach sleep duration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_28_133915) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_01_183449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_28_133915) do
     t.index ["following_id", "followed_id"], name: "idx_follows_following_id_followed_id", unique: true
   end
 
+  create_table "sleep_records", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "clock_in", null: false
+    t.datetime "clock_out"
+    t.integer "sleep_duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "clock_in"], name: "idx_sleep_records_user_clock_in", unique: true
+    t.index ["user_id"], name: "index_sleep_records_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "password_digest", null: false
@@ -29,4 +40,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_28_133915) do
     t.datetime "updated_at", null: false
     t.index ["name"], name: "idx_users_on_name", unique: true
   end
+
+  add_foreign_key "sleep_records", "users"
 end
