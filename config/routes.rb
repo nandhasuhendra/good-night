d020152fd7bf0a8c7601547f78e7ffe979ba20aa
nanda.suhendra <nandhasuhendra@gmail.com>
2025-03-02 01:54:09 +0700

    feat: Adjust routes to get sleep histories from a user itself or from the followed users

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,14 @@ Rails.application.routes.draw do
 
       resource :user do
         collection do
-          get :followings, to: "user/followings#index"
-          get "followings/sleep_histories", to: "user/sleep_histories#index" # Only followed by user can see the sleep histories
-
           get :followers, to: "user/followers#index"
+          get :followings, to: "user/followings#index"
+          get "followings/sleep_histories", to: "user/following_sleep_histories#index" # Only followed by user can see the sleep histories
+
           post :clock_in, to: "user/sleep_records#create"
           patch :clock_out, to: "user/sleep_records#update"
+
+          get :sleep_histories, to: "user/sleep_histories#index"
         end
       end
 
