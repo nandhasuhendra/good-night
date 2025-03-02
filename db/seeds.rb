e787370e047de696c5bcca9d39894498c20cb753
nanda.suhendra <nandhasuhendra@gmail.com>
2025-03-02 23:06:56 +0700

    chore: Create dummy data for sleep records

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,9 +31,27 @@ end
   { followed_id: 2, following_id: 4 },
   { followed_id: 2, following_id: 5 },
   { followed_id: 3, following_id: 1 },
-  { followed_id: 3, following_id: 2 }
+  { followed_id: 3, following_id: 2 },
+  { followed_id: 3, following_id: 4 },
+  { followed_id: 3, following_id: 5 },
+  { followed_id: 4, following_id: 1 },
+  { followed_id: 4, following_id: 2 },
+  { followed_id: 4, following_id: 3 },
+  { followed_id: 4, following_id: 5 },
+  { followed_id: 5, following_id: 1 },
+  { followed_id: 5, following_id: 2 },
+  { followed_id: 5, following_id: 3 },
+  { followed_id: 5, following_id: 4 }
 ].each do |follow|
   Follow.find_or_create_by!(followed_id: follow[:followed_id], following_id: follow[:following_id])
+end
+
+%w[Alice Bob Charlie David Eve].each do |name|
+  puts "Seeding sleep records for #{name}"
+  1_000.times do |i|
+    SleepRecord.create!(user_id: User.find_by(name: name).id, clock_in: Time.now - i.day, clock_out: Time.now - i.day + rand(8).hours)
+  end
+  puts "Seeding sleep records for #{name} finished"
 end
 
 puts "Seeded users finished"
