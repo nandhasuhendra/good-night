fdb7f0a2de5c6a1905c0801cb45178a880b192f1
nanda.suhendra <nandhasuhendra@gmail.com>
2025-03-02 11:33:10 +0700

    feat: Create an action for clock_out

--- app/controllers/api/v1/user/sleep_records_controller.rb
+++ app/controllers/api/v1/user/sleep_records_controller.rb
@@ -1,11 +1,25 @@
 module API
   module V1
     module User
-      class SleepHistoriesController < ApplicationController
+      class SleepRecordsController < ApplicationController
         def create
+          @result = ::SleepRecords::ClockInService.call(current_user)
+          unless @result.success?
+            render json: { errors: @result.result }, status: :unprocessable_entity
+            return
+          end
+
+          @result = @result.result[:record]
         end
 
         def update
+          @result = ::SleepRecords::ClockOutService.call(current_user)
+          unless @result.success?
+            render json: { errors: @result.result }, status: :unprocessable_entity
+            return
+          end
+
+          @result = @result.result[:record]
         end
       end
     end
--- app/services/error_messages.rb
+++ app/services/error_messages.rb
@@ -1,6 +1,11 @@
 module ErrorMessages
-  # Follow error messages
+  # ==> Follow error messages
   def em_follow_not_found
-    I18n.t('activerecord.errors.messages.follow.data_not_found')
+    I18n.t('errors.messages.follow.not_found')
+  end
+
+  # ==> Sleep record error messages
+  def em_sleep_records_not_found
+    I18n.t('errors.messages.sleep_record.not_found')
   end
 end
