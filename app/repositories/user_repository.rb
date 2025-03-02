cd9689c3b2923ef79d56f31bc55e074d0fbee149
nanda.suhendra <nandhasuhendra@gmail.com>
2025-03-02 11:50:37 +0700

    chore: Add order by as argument

--- app/repositories/user_repository.rb
+++ app/repositories/user_repository.rb
@@ -1,9 +1,9 @@
 class UserRepository
-  def self.followers(user)
-    user.followers.order(created_at: :desc)
+  def self.followers(user, order_by: :desc)
+    user.followers.order(created_at: order_by)
   end
 
-  def self.following(user)
-    user.following.order(created_at: :desc)
+  def self.following(user, order_by: :desc)
+    user.following.order(created_at: order_by)
   end
 end
