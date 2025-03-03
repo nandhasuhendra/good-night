46df6ffa841c66ed805af04ef6997c75386b8b04
nanda.suhendra <nandhasuhendra@gmail.com>
2025-03-03 10:50:17 +0700

    feat: Create and improve sleep_histories endpoint

--- app/controllers/api/v1/user/following_sleep_histories_controller.rb
+++ app/controllers/api/v1/user/following_sleep_histories_controller.rb
@@ -5,9 +5,15 @@ module API
         include Pagy::Backend
 
         def index
-          following_user_ids = current_user.following.pluck(:id)
-          @sleep_histories = SleepRecords::WeeklyHistoriesService.call(current_user.id, following_user_ids, params[:week_start])
-          @pagy, @sleep_histories = pagy(@sleep_histories)
+          result = SleepRecords::WeeklyHistoriesService.call(
+            current_user,
+            params[:week_start],
+            page: params[:page],
+            per_page: params[:per_page] || Pagy::DEFAULT[:limit]
+          )
+
+          @pagy = result[:pagy]
+          @sleep_histories = result[:sleep_histories]
         end
       end
     end
--- app/services/sleep_records/weekly_histories_service.rb
+++ app/services/sleep_records/weekly_histories_service.rb
@@ -1,23 +1,68 @@
 module SleepRecords
   class WeeklyHistoriesService < ApplicationService
-    def initialize(user_id, user_ids, week_start)
-      @user_id    = user_id
-      @user_ids   = user_ids
+    include Pagy::Backend
+
+    BATCH_SIZE = 500
+
+    def initialize(user, week_start, options = {})
+      @user       = user
       @week_start = week_start || Date.today.beginning_of_week
+      @options    = options
     end
 
     def call
-      fetch_sleep_histories
+      sleep_histories = fetch_sleep_histories
+      Rails.cache.write(cache_key, sleep_histories, expires_in: 1.hour)
+      pagy, sleep_histories = pagy_array(sleep_histories, page: page, items: per_page)
+
+      { pagy: pagy, sleep_histories: sleep_histories }
     end
 
     private
 
+    def page
+      (@options[:page] || 1).to_i
+    end
+
+    def per_page
+      (@options[:per_page] || Pagy::DEFAULT[:limit]).to_i
+    end
+
+    def cache_key
+      week_number = @week_start.strftime("%Y_%W")
+      "weekly_histories:user_#{@user.id}:week_#{week_number}:page_#{page}:per_page_#{per_page}"
+    end
+
     def fetch_sleep_histories
-      SleepRecord.joins(:user)
-                 .select('users.name AS user_name', 'sleep_records.*')
-                 .where(user_id: @user_ids)
-                 .where('clock_in::DATE >= ? AND clock_in::DATE < ?', @week_start, @week_start + 7.days)
-                 .order(sleep_duration: :desc)
+      return Rails.cache.read(cache_key) if Rails.cache.exist?(cache_key)
+
+      following_user_ids = @user.following.pluck(:id)
+      return [] if following_user_ids.blank?
+
+      sleep_histories = fetch_data_from_database(following_user_ids)
+      return [] if sleep_histories.blank?
+
+      sleep_histories
+    end
+
+    def fetch_data_from_database(following_user_ids)
+      sleep_records = []
+
+      # Use batch processing to avoid SQL issue `WHERE attr_ids IN (...) Too many` fetching all sleep records at once
+      following_user_ids.each_slice(BATCH_SIZE) do |user_ids|
+        sleep_records += fetch_data_from_database_query(user_ids)
+      end
+
+      sleep_records.sort_by! { |record| -record.sleep_duration.to_i }
+    end
+
+    def fetch_data_from_database_query(user_ids)
+      SleepRecord
+        .joins(:user)
+        .select("users.name AS user_name", "sleep_records.*")
+        .where(user_id: user_ids)
+        .where("clock_in::DATE >= ? AND clock_in::DATE < ?", @week_start, @week_start + 7.days)
+        .order(sleep_duration: :desc)
     end
   end
 end
